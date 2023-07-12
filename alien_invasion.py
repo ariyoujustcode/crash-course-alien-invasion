a399722d70180ae2fa0d41e32b18f275acd209c5
ariyoujustcode <arimarine23@gmail.com>
2023-07-11 20:08:29 -0500

    Finished the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,6 +72,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -143,6 +145,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+        # Increase level.
+        self.stats.level += 1
+        self.sb.prep_level()
+
     def _update_aliens(self):
         """
         Check if a fleet is at the edge, 
@@ -204,8 +210,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
