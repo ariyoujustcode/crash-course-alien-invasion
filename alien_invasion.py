b1e22447c418e5c44b84d7b29e29c0336c1e9fbc
ariyoujustcode <arimarine23@gmail.com>
2023-07-11 19:24:18 -0500

    Highscore and rounded up added.

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -129,6 +130,12 @@ class AlienInvasion:
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
               self.bullets, self.aliens, True, True)
+        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
